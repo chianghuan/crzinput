a9a3d4081d06332f99eba7370bf1235fa2903f3d
huanjiang <huanchiang@gmail.com>
2013-01-24 02:21:48 +0800

    Initial Commit

---
+++ CrzInput/CrzInputAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CrzInputAppDelegate.h
+//  CrzInput
+//
+//  Created by huanjiang on 13-1-24.
+//  Copyright (c) 2013年 huanjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrzInputAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrzInput/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrzInput
+//
+//  Created by huanjiang on 13-1-24.
+//  Copyright (c) 2013年 huanjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CrzInputAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CrzInputAppDelegate class]));
+    }
+}
